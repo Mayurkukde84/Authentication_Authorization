bce95b66d51cdb8dc54780351a3ffffcf3c7db68
mayur <mayurkukde84@gmail.com>
2023-03-01 19:55:56 +0530

    update

--- server.js
+++ server.js
@@ -6,12 +6,37 @@ const mongoose = require('mongoose')
 const connectDB = require('./config/connectdb')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
 PORT = process.env.PORT
 mongoose.set("strictQuery", true);
 connectDB()
-app.use(express.json())
+app.use(helmet()) //set security http headers
+const limiter = rateLimit({
+    max:100,
+    windowMs: 60 * 60*1000,
+    message:'Toomany request from this IP, please try again in an hour'
+  })
+  app.use('/api',limiter)
+app.use(express.json({limit:'10kb'}))
 app.use(cors())
 app.use(cookieParser())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp({
+  whitelist:[
+    "duration",
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+}))
+
+  
 app.use('/',express.static(path.join(__dirname,'public')))
 app.use('/',require('./routes/root'))
 app.use('/api/v1/auth',require('./routes/authRoutes'))
